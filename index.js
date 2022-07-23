8c56dfd6d539c5d242162d62e53f2285438fe71a
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-07-23 07:23:13 +0600

    user connection done

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const port = 5000 || process.env.PORT;
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-server.listen(port, () => {
-  console.log(`Listening to the port ${port}`);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(5000, () => {
+  console.log("listening on *:5000");
 });
